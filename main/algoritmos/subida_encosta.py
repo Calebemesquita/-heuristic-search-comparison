bc97b9d42e638e1106470758a5b0d0c1f0471fc4
Calebemesquita <calebemesquita.13@gmail.com>
2026-07-03 10:43:19 -0300

    refactor/doc: documentacao melhoria do algoritmo

--- main/algoritmos/subida_encosta.py
+++ main/algoritmos/subida_encosta.py
@@ -5,44 +5,43 @@ from typing import List, Optional, Tuple
 from main.algoritmos.best_first_search import HeuristicaFn, Ponto, RetornoBusca
 from main.mapa import vizinhos
 
-
-def sde_gulosa_deterministica(
-    mapa,
-    origem: Ponto,
-    destino: Ponto,
-    heuristica_fn: HeuristicaFn,
-) -> RetornoBusca:
-
-
+"""
+# algoritmo de <Subida de Encosta>
+variação: subida de encosta First Choice
+Intera nos vizinhos na ordem N-L-S-O
+No primeiro vizinho que Heuristica H(atual) > Hvizinho) ele se move para o vizinho
+"""
+def subidaEncostaFirstChoice( mapa, origem: Ponto,  destino: Ponto, heuristica_fn: HeuristicaFn,) -> RetornoBusca:
     n = len(mapa)
-    atual_pos: Ponto = origem
-    atual_h: float = heuristica_fn(origem, destino)
-    atual_g: float = 0
-    caminho: List[Ponto] = [origem]
+    current_possition: Ponto = origem
+    current_heuristic: float = heuristica_fn(origem, destino)
+    
+    custo_acumulado: float = 0
+    current_path: List[Ponto] = [origem]
 
     estados_gerados = 1
     estados_visitados = 0
 
-    while atual_pos != destino:
+    while current_possition != destino:
         estados_visitados += 1
         melhorou = False
 
-        for v in vizinhos(atual_pos, mapa, n):
+        for v in vizinhos(current_possition, mapa, n):
             estados_gerados += 1
-            v_h = heuristica_fn(v, destino)
+            heuristic_next_vertice = heuristica_fn(v, destino)
 
-            if v_h < atual_h:
-                atual_g += mapa[v[0]][v[1]]
-                atual_h = v_h
-                atual_pos = v
-                caminho.append(v)
-                melhorou = True
+            if heuristic_next_vertice < current_heuristic:
+                custo_acumulado += mapa[v[0]][v[1]]                     #somamos o custo do vertice que encontramos melhor
+                current_heuristic = heuristic_next_vertice              #atualizamos a heuristica
+                current_possition = v                                   #atualiza posicao atual
+                current_path.append(v)                                  #adiciona o vertice ao caminho
+                melhorou = True                                         #quebramos o loop, pois encontramos um vizinho melhor
                 break
 
         if not melhorou:
-            return None, None, estados_gerados, estados_visitados
+            return None, None, estados_gerados, estados_visitados       #desiste
 
-    return caminho, int(atual_g), estados_gerados, estados_visitados
+    return current_path, int(custo_acumulado), estados_gerados, estados_visitados
 
 
 def sde_maior_aclive(
@@ -54,36 +53,37 @@ def sde_maior_aclive(
 
 
     n = len(mapa)
-    atual_pos: Ponto = origem
-    atual_h: float = heuristica_fn(origem, destino)
-    atual_g: float = 0
-    caminho: List[Ponto] = [origem]
+    current_possition: Ponto = origem
+    current_heuristic: float = heuristica_fn(origem, destino)
+
+    custo_acumulado: float = 0
+    current_path: List[Ponto] = [origem]
 
     estados_gerados = 1
     estados_visitados = 0
 
-    while atual_pos != destino:
+    while current_possition != destino:
         estados_visitados += 1
-        melhor_v: Optional[Ponto] = None
-        melhor_h: float = atual_h
+        best_vertice: Optional[Ponto] = None
+        best_heuristic: float = current_heuristic
 
-        for v in vizinhos(atual_pos, mapa, n):
+        for v in vizinhos(current_possition, mapa, n):
             estados_gerados += 1
             v_h = heuristica_fn(v, destino)
 
-            if v_h < melhor_h:
-                melhor_h = v_h
-                melhor_v = v
+            if v_h < best_heuristic:
+                best_heuristic = v_h
+                best_vertice = v
 
-        if melhor_v is None:
+        if best_vertice is None:
             return None, None, estados_gerados, estados_visitados
 
-        atual_g += mapa[melhor_v[0]][melhor_v[1]]
-        atual_h = melhor_h
-        atual_pos = melhor_v
-        caminho.append(melhor_v)
+        custo_acumulado += mapa[best_vertice[0]][best_vertice[1]]
+        current_heuristic = best_heuristic
+        current_possition = best_vertice
+        current_path.append(best_vertice)
 
-    return caminho, int(atual_g), estados_gerados, estados_visitados
+    return current_path, int(custo_acumulado), estados_gerados, estados_visitados
 
 
 def sde_gulosa_estocastica(
